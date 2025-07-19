ae0988fddcfc3186405dff4d22cdd1134024ea58
jangyeongwoo <sbtmxhs@gmail.com>
2025-07-19 19:17:50 +0900

    #1 프로젝트 초기 설정  - build-logic 디렉토리를 독립적인 빌드로 포함  - build-logic:settings.gradle 추가  - build-logic:gradle.properties 추가  - enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS") 설정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
         google {
             content {
@@ -19,9 +20,14 @@ dependencyResolutionManagement {
     }
 }
 
+//_ 멀티 모듈 프로젝트에서 다른 모듈에 의존성을 추가할 때, 문자열 대신 타입 안전한 방식으로 접근할 수 있도록 함.
+//_ projects.core.model과 같은 형식으로 모듈 참조 가능
+//_ 문자열 기반 참조(project(":core:model")) 대신 타입 안전한 참조 제공
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 rootProject.name = "SayeongApp"
+
 include(":app")
 include(":core:data")
 include(":core:domain")
 include(":feature:Home")
-include(":build-logic:convention")
