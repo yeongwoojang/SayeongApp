3e922601847e67ac5f6e30b10a75415474753bd6
jangyeongwoo <sbtmxhs@gmail.com>
2025-08-05 00:17:17 +0900

    #1 프로젝트 초기 설정 - 공통 테마 적용 3차 (커스텀 색상 사용 하도록 수정)

--- core/designsystem/src/main/java/com/sayeong/vv/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/sayeong/vv/designsystem/theme/Theme.kt
@@ -79,7 +79,7 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun SayeongAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
