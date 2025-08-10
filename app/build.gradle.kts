72e747208593d25dd55c45481af44ee06f964fd8
jangyeongwoo <sbtmxhs@gmail.com>
2025-08-10 16:34:12 +0900

    - Hilt관련 빌드에러 수정 (hilt 버전 2.49 -> 2.56)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.sayeong.android.application)
-    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.sayeong.android.hilt)
     alias(libs.plugins.sayeong.android.compose)
 }
@@ -15,7 +14,6 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
--- build-logic/convention/src/main/java/HiltConventionPlugin.kt
+++ build-logic/convention/src/main/java/HiltConventionPlugin.kt
@@ -1,6 +1,7 @@
 import com.sayeong.vv.convention.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.dependencies
 
 class HiltConventionPlugin: Plugin<Project> {
@@ -11,10 +12,6 @@ class HiltConventionPlugin: Plugin<Project> {
                 apply("com.google.devtools.ksp")
             }
 
-            dependencies {
-                "ksp"(libs.findLibrary("hilt.compiler").get())
-            }
-
             pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
                 dependencies {
                     "implementation"(libs.findLibrary("hilt.core").get())
@@ -23,10 +20,14 @@ class HiltConventionPlugin: Plugin<Project> {
 
             pluginManager.withPlugin("com.android.base") {
                 pluginManager.apply("com.google.dagger.hilt.android")
+                pluginManager.apply( "dagger.hilt.android.plugin")
+
                 dependencies {
                     "implementation"(libs.findLibrary("hilt.android").get())
+                    "ksp"(libs.findLibrary("hilt.compiler").get())
                 }
             }
+
         }
     }
 }
